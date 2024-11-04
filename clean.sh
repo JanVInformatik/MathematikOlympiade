30091d7d4b61af1bedfc171dc871f0adbaa3c098
JanV <janvogele2007@icloud.com>
2024-11-04 02:26:33 +0100

    Hinzufuegen eines Clean Skripts (.sh).

---
+++ clean.sh
@@ -0,0 +1,8 @@
+rm -rf *.hi
+rm -rf *.o
+rm -rf *.out
+rm -rf main
+rm -rf main2Debug
+rm -rf main3Debug
+
+# Entfernt alle Dateien, die zum Kompilieren, Linken oder Ausfuehren notwendig sind bzw. waren.
